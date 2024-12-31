e5c957e159c6cbd38ddf6218076f58f9cf3cb81a
{username} <ossama>
2024-12-31 14:03:45 +0100

    Helo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    images: {
+        unoptimized: true, // Desactiva la optimización de imágenes para sitios estáticos.
+    },
 };
 
 export default nextConfig;
